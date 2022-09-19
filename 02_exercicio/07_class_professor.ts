12c0c3fc4101fdfad41deb60a531044b4ca416f3
Gabriel Morais <gabriel.am.mor@gmail.com>
2022-09-19 12:24:15 -0300

    ordinary changes

---
+++ 02_exercicio/07_class_professor.ts
@@ -0,0 +1,12 @@
+class Professor {
+    name : string = "";
+    paymentTime : string = "";
+    preferredLang : string = "";
+}
+
+const profEly = new Professor();
+profEly.name = "Ely";
+profEly.paymentTime = "12.56";
+profEly.preferredLang = "Typescript";
+
+console.log(`${profEly.name}\nMy paymente time is ${profEly.paymentTime}\nand\nMy preferred language is ${profEly.preferredLang}`);
