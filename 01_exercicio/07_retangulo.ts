f718f84a60b31c3b336e7bb6ee5031eb6b14e7ec
Gabriel Morais <gabriel.am.mor@gmail.com>
2022-09-19 11:13:01 -0300

    ordinary changes

--- 01_exercicio/07_retangulo.ts
+++ 01_exercicio/07_retangulo.ts
@@ -1,19 +1,19 @@
 class Retangulo {
-    base: number = 0
-    altura: number = 0
+    base: number = 0;
+    altura: number = 0;
 
     calcularArea(): number {
-        return this.base * this.altura
+        return this.base * this.altura;
     }
     
     calcularPerimetro(): number {
-        return (this.base + this.altura) * 2
+        return (this.base + this.altura) * 2;
     }
 }
 
-const retangulo1 = new Retangulo
-retangulo1.base = 5
-retangulo1.altura = 3
+const retangulo1 = new Retangulo;
+retangulo1.base = 5;
+retangulo1.altura = 3;
 
-console.log(`Área do retângulo: ${retangulo1.calcularArea()}`)
-console.log(`Perímetro do retângulo: ${retangulo1.calcularPerimetro()}`)
+console.log(`Área do retângulo: ${retangulo1.calcularArea()}`);
+console.log(`Perímetro do retângulo: ${retangulo1.calcularPerimetro()}`);
--- 01_exercicio/09_situacao_financeira.ts
+++ 01_exercicio/09_situacao_financeira.ts
@@ -1,14 +1,14 @@
 class SituacaoFinanceira {
-    valorCreditos: number = 0
-    valorDebitos: number = 0
+    valorCreditos: number = 0;
+    valorDebitos: number = 0;
 
     saldo(): number {
-        return this.valorCreditos - this.valorDebitos
+        return this.valorCreditos - this.valorDebitos;
     }
 }
 
-let situacaoFinanceira1 = new SituacaoFinanceira
-situacaoFinanceira1.valorCreditos = 1200
-situacaoFinanceira1.valorDebitos = 599
+let situacaoFinanceira1 = new SituacaoFinanceira;
+situacaoFinanceira1.valorCreditos = 1200;
+situacaoFinanceira1.valorDebitos = 599;
 
-console.log(`Saldo: R$ ${situacaoFinanceira1.saldo().toFixed(2)}`)
+console.log(`Saldo: R$ ${situacaoFinanceira1.saldo().toFixed(2)}`);
